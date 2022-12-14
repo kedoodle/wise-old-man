659edbd066b5cd7125821499e9ade4ee2ae8073d
Keddy Shen <hello@keddy.dev>
2022-12-14 13:23:38 +1100

    add Client struct and func to produce a new Client

---
+++ wiseoldman.go
@@ -0,0 +1,15 @@
+package wiseoldman
+
+import "net/http"
+
+type Client struct {
+	httpClient *http.Client
+	baseURL    string
+}
+
+func New() *Client {
+	return &Client{
+		httpClient: http.DefaultClient,
+		baseURL:    "https://api.wiseoldman.net/v2",
+	}
+}
